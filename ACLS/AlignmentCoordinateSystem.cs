00ef1532b9953e25d6451381158b357487ea8b6e
qwerty1423 <120289450+qwerty1423@users.noreply.github.com>
2026-02-23 23:38:09 +1100

    return to default

--- ACLS/AlignmentCoordinateSystem.cs
+++ ACLS/AlignmentCoordinateSystem.cs
@@ -13,18 +13,20 @@ public class AlignmentCoordinateSystem
     public void UpdateFromAlignment(Vector3 alignmentVector)
     {
         Forward = alignmentVector.normalized;
-        Right = Vector3.Cross(Vector3.up, Forward).normalized;
-        Up = Vector3.Cross(Forward, Right).normalized;
+        Vector3 val = new(Forward.z, 0f, 0f - Forward.x);
+        Right = val.normalized;
+        Up = Vector3.Cross(Forward, Right);
     }
 
     public (float yaw, float pitch, float roll) GetRelativeAngles(Vector3 direction, Quaternion rotation)
     {
-        Vector3 localDir = Quaternion.Inverse(Quaternion.LookRotation(Forward, Up)) * direction.normalized;
-        float yaw = Mathf.Atan2(localDir.x, localDir.z) * Mathf.Rad2Deg;
-        float pitch = -Mathf.Atan2(localDir.y, localDir.z) * Mathf.Rad2Deg;
-        Vector3 aircraftUp = rotation * Vector3.up;
-        Vector3 localUp = Quaternion.Inverse(Quaternion.LookRotation(Forward, Up)) * aircraftUp;
-        float roll = Mathf.Atan2(localUp.x, localUp.y) * Mathf.Rad2Deg;
-        return (yaw, pitch, roll);
+        direction = direction.normalized;
+        Vector3 val = Vector3.ProjectOnPlane(direction, Up);
+        float item = Vector3.SignedAngle(Forward, val, Up);
+        float item2 = Vector3.SignedAngle(direction, val, Right);
+        Vector3 val2 = rotation * Vector3.up;
+        Vector3 val3 = Vector3.ProjectOnPlane(val2, Forward);
+        float item3 = Vector3.SignedAngle(val3, Up, Forward);
+        return (yaw: item, pitch: item2, roll: item3);
     }
 }

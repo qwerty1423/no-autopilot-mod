a63824c4f5b6d904ff5059f18292e1df8087438d
qwerty1423 <120289450+qwerty1423@users.noreply.github.com>
2026-04-16 02:32:00 +1000

    pid controllers from mechjeb

---
+++ Core/PID/IPIDLoop.cs
@@ -0,0 +1,10 @@
+/*
+ * Copyright Lamont Granquist, Sebastien Gaggini and the MechJeb contributors
+ * SPDX-License-Identifier: LicenseRef-PD-hp OR Unlicense OR CC0-1.0 OR 0BSD OR MIT-0 OR MIT OR LGPL-2.1+
+ */
+namespace NOAutopilot.Core.PID;
+
+public interface IPIDLoop
+{
+    double Update(double r, double y);
+}

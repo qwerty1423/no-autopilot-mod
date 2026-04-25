33da1d1ea4dfa937df8c0f0b1b869aa7696c6d43
qwerty1423 <120289450+qwerty1423@users.noreply.github.com>
2026-04-25 20:53:27 +1000

    give explanation for patch

--- Core/Flight/FixGLOCLeakPatch.cs
+++ Core/Flight/FixGLOCLeakPatch.cs
@@ -10,6 +10,16 @@ using Object = UnityEngine.Object;
 
 namespace NOAutopilot.Core.Flight;
 
+/// <summary>
+/// Fixes a GLOC component leak in <see cref="PilotPlayerState.EnterState"/> from autoland.
+/// <para>
+/// The base game adds a new GLOC component every time the player
+/// enters the pilot state without checking for an existing instance, and
+/// <see cref="PilotPlayerState.LeaveState"/> never destroys the component.
+/// This causes GLOC components to stack on the pilot GameObject each time
+/// autoland is triggered without this patch.
+/// </para>
+/// </summary>
 [HarmonyPatch(typeof(PilotPlayerState), "EnterState")]
 internal static class FixGLOCLeakPatch
 {
@@ -26,7 +36,7 @@ internal static class FixGLOCLeakPatch
             GLOC existingGloc = pilot.gameObject.GetComponent<GLOC>();
             if (existingGloc != null)
             {
-                Object.DestroyImmediate(existingGloc);
+                Object.Destroy(existingGloc);
             }
         }
         catch (Exception ex)
